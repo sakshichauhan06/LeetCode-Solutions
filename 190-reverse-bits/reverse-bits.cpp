c6f863babc60f70f6fae6bcf68db571f05e5e9c5
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-08-24 20:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (25.59%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+    bool isItSet(int i, uint32_t n) {
+        return n & (1 << i);
+    }
+
     uint32_t reverseBits(uint32_t n) {
-        uint32_t ans = 0;
-        for(int i = 0; i < 31; i++) {
-            ans = (n % 2) + ans << 1;
-            n >>= 1;
+        int ans = 0;
+        for(int i = 0; i <= 31; i++) {
+            if(isItSet(i, n)) {
+                ans = (1 << (31 - i)) | ans;
+            }
         }
-        return ans + n % 2;
+        return ans;
     }
 };
