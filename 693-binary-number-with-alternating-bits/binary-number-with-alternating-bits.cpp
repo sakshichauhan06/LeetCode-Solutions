29c84c4fab75f13a2a83713199a50ec206757a5d
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-08-26 11:51:34 +0530

    Time: 3 ms (27.53%) | Memory: 6.1 MB (17.42%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,7 +1,15 @@
+// class Solution {
+//  public:
+//   bool hasAlternatingBits(uint32_t n) {
+//     uint32_t validator = n + (n >> 1);
+//     return (validator & validator + 1) == 0;
+//   }
+// };
+
 class Solution {
- public:
-  bool hasAlternatingBits(uint32_t n) {
-    uint32_t validator = n + (n >> 1);
-    return (validator & validator + 1) == 0;
-  }
+public:
+    bool hasAlternatingBits(uint32_t n) {
+        uint32_t x = n + (n >> 1);
+        return (x & x + 1) == 0;
+    }
 };
