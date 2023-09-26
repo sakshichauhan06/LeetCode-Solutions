e35a1864898abc56fdc255dda2f1f58fec173967
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-09-26 14:36:50 +0530

    Time: 27 ms (55.61%) | Memory: 22.6 MB (37.29%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        vector<int> vis(n, 0);
         int i = 0;
+        vector<int> vis(n, false);
+        vector<int> ans;
 
         while(i < n) {
             if(!vis[i] && nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
                 swap(nums[i], nums[nums[i] - 1]);
             } else {
-                vis[i] = 1;
+                vis[i] = true;
                 i++;
             }
         }
 
-        vector<int> ans;
-
         for(int i = 0; i < n; i++) {
             if(nums[i] != i + 1) {
                 ans.push_back(nums[i]);
-                ans.push_back(i+1);
+                ans.push_back(i + 1);
                 break;
             }
         }
