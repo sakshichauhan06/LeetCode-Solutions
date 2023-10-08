5509318b05aa081ad05d3164353134fa29c58a9e
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-10-08 19:19:06 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (11.92%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
+    long long solve(long long n) {
+        long long k = (n * (n + 1)) / 2;
+        return k;
+    }
     int arrangeCoins(int n) {
-        int lb=1,ub=n;
-        long mid=0;
-        while(lb<=ub)
-        {
-            mid=lb+(ub-lb)/2;
-            long total=(mid*(mid+1))/2;
-            if(total==n)
+        long long low = 1, high = n;
+
+        while(low <= high) {
+            long long mid = low + (high - low) / 2;
+            long long sum = solve(mid);
+            if(sum == n) {
                 return mid;
-            else if(total<n)
-                lb=mid+1;
-            else
-                ub=mid-1;
+            } else if(sum < n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        return ub;
+
+        return high;
+
     }
 };
