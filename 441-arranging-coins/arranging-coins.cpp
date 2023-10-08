86e560fad76f9fa73034d6789f43ced56ccb9bd1
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-10-08 19:17:14 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (11.92%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int lb=1,ub=n;
+        long mid=0;
+        while(lb<=ub)
+        {
+            mid=lb+(ub-lb)/2;
+            long total=(mid*(mid+1))/2;
+            if(total==n)
+                return mid;
+            else if(total<n)
+                lb=mid+1;
+            else
+                ub=mid-1;
+        }
+        return ub;
+    }
+};
