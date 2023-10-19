e8829492bfb7f2d1ad96e00a40d9f4eb111e8594
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-10-19 12:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (53.12%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int numberOfSteps(int num) {
-        int count = 0;
-
-        while(num) {
-            if(num & 1) {
-                num = num ^ 1;
-            } else {
-                num = num >> 1;
-            }
-            count++;
+    int helper(int n, int count) {
+        if(n == 0) {
+            return count;
         }
+        if(n % 2 == 0) {
+            return helper(n / 2, count + 1);
+        }
+        return helper(n - 1, count + 1);
+    }
 
-        return count;
+    int numberOfSteps(int num) {
+        return helper(num, 0);
     }
 };
