1cfd2d5cff30fe3bee51e4903c06151db8aec616
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-12-04 17:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (96.85%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n > 0 && (n & (n - 1)) == 0 && (n & 0xAAAAAAAA) == 0) {
+        if(n == 1) {
             return true;
         }
-        return false;
+        if(n % 4 != 0 || n <= 0) {
+            return false;
+        }
+        return isPowerOfFour(n / 4);
     }
 };
