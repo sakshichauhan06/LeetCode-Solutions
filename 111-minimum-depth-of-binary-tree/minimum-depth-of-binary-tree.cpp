f82873222d418a9d75925e45ce61c410dc3f2f5d
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2023-12-20 09:04:55 +0530

    Time: 199 ms (69.19%) | Memory: 146.5 MB (99.00%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getMini(TreeNode* root, int len) {
+        if(root == nullptr) {
+            return 0;
+        } else if(root != nullptr && root->left == nullptr && root->right != nullptr) {
+            return getMini(root->right, len + 1);
+        } else if(root != nullptr && root->left != nullptr && root->right == nullptr){
+            return getMini(root->left, len + 1);
+        } else if(root != nullptr && root->left == nullptr && root->right == nullptr) {
+            return len;
+        } else if(root != nullptr) {
+            return min(getMini(root->left, len + 1), getMini(root->right, len + 1));
+        }
+        return (len + 1);
+    }
+
+    int minDepth(TreeNode* root) {
+        if(root == nullptr) {
+            return 0;
+        }
+
+        int ans = getMini(root, 1);
+
+        return ans;
+    }
+};
