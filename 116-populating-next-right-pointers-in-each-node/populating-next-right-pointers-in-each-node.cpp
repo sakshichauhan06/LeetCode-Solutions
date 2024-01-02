b847944c42f5e127cd097742fad52a434f5b3edf
Sakshi Chauhan <57169188+sakshichauhan06@users.noreply.github.com>
2024-01-02 08:55:19 +0530

    Time: 27 ms (5.02%) | Memory: 17.3 MB (58.07%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int len = q.size();
+            Node* temp = nullptr;
+            
+            for (int i = 0; i < len; i++) {
+                temp = q.front();
+                q.pop();
+
+                if (i == len - 1) {
+                    temp->next = nullptr;
+                }
+
+                if (temp->left) {
+                    q.push(temp->left);
+
+                    if (temp->right) {
+                        q.push(temp->right);
+                        temp->left->next = temp->right;
+                    }
+
+                    if (temp) {
+                        temp->right->next = temp->next ? temp->next->left : nullptr;
+                    }
+                }
+            }
+        }
+
+        return root;
+    }
+};
